03bc8799622e9346e2a5906114a9d8d5f8c6c83a
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-28 14:54:17 +1100

    SWEATMAN, Samuel: 2018-02-28 14:54:17

--- user/calculator.py
+++ user/calculator.py
@@ -1,11 +1,50 @@
+# adding equation
 def add(x, y):
     return x + y
 
+#subtracting equation
+def sub(x, y):
+    return x - y
+#multiply equatin
+def times(x, y):
+    return x * y
+
+#dividing equation
+def div(x, y):
+    return x / y
+
+#to the power of equation
+def powerof(x ,y):
+    return x ** y
 
 
+print('select operation:')
+print('1. Add')
+print('2. subtract')
+print('3. multiply')
+print('4. divide')
+print('5. raise the number to the power of')
 
-num1 = float(input('Enter your first number: '))
+choice=input('enter 1,2,3,4,5: ')
 
-num2 = float(input('Enter your second number: '))
+num1=float(input('Enter first number: '))
+num2=float(input('Enter second number: '))
 
-print(num1, '+', num2, '=', add(num1, num2))
+if choice == '1':
+    answer=add(num1, num2)
+    print(num1, ' + ', num2, ' = ', answer)
+elif choice == '2':
+    answer=sub(num1, num2)
+    print(num1, ' - ',num2, ' = ', answer)
+elif choice == '3':
+    answer=times(num1, num2)
+    print(num1, ' * ', num2, ' = ', answer)
+elif choice == '4':
+    answer=div(num1, num2)
+    print(num1, ' / ', num2, ' = ', answer)
+elif choice == '5':
+    answer=powerof(num1, num2)
+    print(num1, ' ** ', num2, ' = ', answer)
+
+else:
+    print('Invalid Input')
---
+++ user/magic8.py
@@ -0,0 +1,16 @@
+#ask a question
+answer = input ('Ask me a question')
+from random import randint
+number = (randint(1, 5))
+if number == 1:
+    print('yeah nah')
+if number == 2:
+    print('go have play minecraft now on james rgs realm')
+if number == 3:
+    print('maybe, maybe not')
+if number == 4:
+    print('you will over come it great difficulty')
+if number == 5:
+    print('may the force be with you')
+
+
