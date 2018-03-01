d38257318c0a62f1af7046c11793d92030d25af8
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-01 12:29:57 +1100

    SWEATMAN, Samuel: 2018-03-01 12:29:57

--- user/magic8.py
+++ user/magic8.py
@@ -1,16 +1,49 @@
 #ask a question
+import time
 answer = input ('Ask me a question')
 from random import randint
-number = (randint(1, 5))
+number = (randint(1, 8))
 if number == 1:
-    print('yeah nah')
+    print('________$$$$_______________')
+    print('_______$$__$_______________')
+    print('_______$___$$______________')
+    print('_______$___$$______________')
+    print('_______$$___$$_____________')
+    print('________$____$$____________')
+    print('________$$____$$$__________')
+    print('________$$_____$$__________')
+    print('_________$$______$$________')
+    print('__________$_______$$_______')
+    print('____$$$$$$$________$$______')
+    print('__$$$_______________$$$$$$_')
+    print('_$$____$$$$____________$$$_')
+    print('_$___$$$__$$$____________$$')
+    print('_$$________$$$____________$')
+    print('__$$____$$$$$$____________$')
+    print('__$$$$$$$____$$___________$')
+    print('__$$_______$$$$___________$')
+    print('___$$$$$$$$$__$$_________$$')
+    print('____$________$$$$_____$$$$')
+    print('____$$____$$$$$$____$$$$$$')
+    print('_____$$$$$$____$$__$$')
+    print('_______$_____$$$_$$$')
+    print('________$$$$$$$$$$')
 if number == 2:
-    print('go have play minecraft now on james rgs realm')
+    print('go play minecraft now on james rgs realm')
 if number == 3:
     print('maybe, maybe not')
 if number == 4:
     print('you will over come it great difficulty')
 if number == 5:
     print('may the force be with you')
-
-
+if number == 6:
+    print('certain')
+if number == 7:
+    print('very likely')
+if number == 7:
+    print('definitely')
+if number == 8:
+    print('thinking'), time.sleep(1)
+    print('thinking'), time.sleep(1)
+    print('thinking'), time.sleep(1)
+    print('error')
